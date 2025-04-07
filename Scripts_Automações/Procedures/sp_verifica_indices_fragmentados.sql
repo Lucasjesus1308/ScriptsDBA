a9419fe62f0ee827be7fd20a3e5df16b63d134aa
Lucasjesus1308 <99757568+Lucasjesus1308@users.noreply.github.com>
2025-04-07 00:37:37 -0300

    Update sp_verifica_indices_fragmentados.sql

--- Scripts_Automações/Procedures/sp_verifica_indices_fragmentados.sql
+++ Scripts_Automações/Procedures/sp_verifica_indices_fragmentados.sql
@@ -1,17 +1,19 @@
 -- Drop da procedure se existir
-IF OBJECT_ID('dbo.sp_Monitoramento_Indices_Fragmentados', 'P') IS NOT NULL
-    DROP PROCEDURE dbo.sp_Monitoramento_Indices_Fragmentados;
+IF OBJECT_ID('dbo.sp_checkidx', 'P') IS NOT NULL
+    DROP PROCEDURE dbo.sp_checkidx;
 GO
 
-CREATE PROCEDURE dbo.sp_Monitoramento_Indices_Fragmentados
+CREATE PROCEDURE dbo.sp_checkidx
 AS
 BEGIN
     SET NOCOUNT ON;
 
-    PRINT '====== MONITORAMENTO DE FRAGMENTAÇÃO DE ÍNDICES (SOMENTE ÍNDICES COM NECESSIDADE DE MANUTENÇÃO) ======';
+    PRINT '====== MONITORAMENTO PREMIUM DE FRAGMENTAÇÃO DE ÍNDICES ======';
 
+    -- Coleta índices fragmentados com relevância (índices grandes)
     SELECT
         DB_NAME() AS Nome_Banco,
+        OBJECT_SCHEMA_NAME(ips.object_id) AS Nome_Schema,
         OBJECT_NAME(ips.object_id) AS Nome_Tabela,
         i.name AS Nome_Indice,
         ips.index_id AS ID_Indice,
@@ -20,28 +22,30 @@ BEGIN
         CASE 
             WHEN ips.avg_fragmentation_in_percent >= 30 THEN 'Rebuild Recomendado'
             WHEN ips.avg_fragmentation_in_percent >= 5 THEN 'Reorganize Recomendado'
+            ELSE 'Nenhuma ação necessária'
         END AS Recomendacao,
         -- Comando para Enterprise (com ONLINE = ON)
         CASE 
             WHEN ips.avg_fragmentation_in_percent >= 30 THEN 
-                'ALTER INDEX [' + i.name + '] ON [' + OBJECT_NAME(ips.object_id) + '] REBUILD WITH (ONLINE = ON);'
+                'ALTER INDEX [' + i.name + '] ON [' + OBJECT_SCHEMA_NAME(ips.object_id) + '].[' + OBJECT_NAME(ips.object_id) + '] REBUILD WITH (ONLINE = ON);'
             WHEN ips.avg_fragmentation_in_percent >= 5 THEN 
-                'ALTER INDEX [' + i.name + '] ON [' + OBJECT_NAME(ips.object_id) + '] REORGANIZE;'
-        END AS Comando_Manutencao,
-        -- Comando para versões Standard e outras que não suportam ONLINE = ON
+                'ALTER INDEX [' + i.name + '] ON [' + OBJECT_SCHEMA_NAME(ips.object_id) + '].[' + OBJECT_NAME(ips.object_id) + '] REORGANIZE;'
+            ELSE ''
+        END AS Comando_Enterprise,
+        -- Comando para versões Standard (sem ONLINE = ON)
         CASE 
             WHEN ips.avg_fragmentation_in_percent >= 30 THEN 
-                'ALTER INDEX [' + i.name + '] ON [' + OBJECT_NAME(ips.object_id) + '] REBUILD;'
+                'ALTER INDEX [' + i.name + '] ON [' + OBJECT_SCHEMA_NAME(ips.object_id) + '].[' + OBJECT_NAME(ips.object_id) + '] REBUILD;'
             WHEN ips.avg_fragmentation_in_percent >= 5 THEN 
-                'ALTER INDEX [' + i.name + '] ON [' + OBJECT_NAME(ips.object_id) + '] REORGANIZE;'
-        END AS Comando_Manutencao_Standard
-    FROM sys.dm_db_index_physical_stats(DB_ID(), NULL, NULL, NULL, 'SAMPLED') AS ips
+                'ALTER INDEX [' + i.name + '] ON [' + OBJECT_SCHEMA_NAME(ips.object_id) + '].[' + OBJECT_NAME(ips.object_id) + '] REORGANIZE;'
+            ELSE ''
+        END AS Comando_Standard
+    FROM sys.dm_db_index_physical_stats(DB_ID(), NULL, NULL, NULL, 'LIMITED') AS ips
     INNER JOIN sys.indexes AS i ON ips.object_id = i.object_id AND ips.index_id = i.index_id
     WHERE 
-        ips.database_id = DB_ID()
-        AND i.type_desc <> 'HEAP' -- Exclui tabelas HEAP (sem índice clustered)
-        AND ips.avg_fragmentation_in_percent >= 5 -- Apenas índices que precisam de manutenção
-    ORDER BY 
-        ips.avg_fragmentation_in_percent DESC;
+        ips.index_id > 0 -- Ignora heaps (sem índice)
+        AND ips.page_count > 100 -- Filtro: pega só índices com mais de 100 páginas
+        AND ips.avg_fragmentation_in_percent >= 5 -- Fragmentação relevante
+    ORDER BY ips.avg_fragmentation_in_percent DESC;
 END;
 GO
